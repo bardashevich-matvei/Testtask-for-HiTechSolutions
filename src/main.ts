fd207c8ffe8f03ba0d72d788df93c129e11a7dc7
Matvei <bardashevich.matvei@gmail.com>
2023-09-14 03:37:15 +0200

    some optimisation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { HttpExceptionFilter } from './utils/error-handler.utils';
 import { MongoExceptionFilter } from './utils/mongo-handler.utils';
 
 async function bootstrap() {
-  createDefaultGenres();
+  await createDefaultGenres();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter(), new MongoExceptionFilter());
